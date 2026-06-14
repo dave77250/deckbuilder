b63c1a47bd069e265c81b34027f3461619012ce6
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-06-14 18:13:59 +0000

    Ajout de la vérification si un deck est complet

--- src/model/Deck.ts
+++ src/model/Deck.ts
@@ -2,6 +2,8 @@ import { CardId, Card } from "./Card";
 import { DeckCard, createFrom } from "./DeckCard";
 import { CardCollection } from "./Collection";
 
+const DECK_SIZE = 60;
+
 export class Deck {
     private readonly cards = new Map<CardId, DeckCard>();
     private readonly existingCards = new Map<CardId, Card>();
@@ -15,5 +17,19 @@ export class Deck {
 
     public getCard(id: CardId) {
         return this.cards.get(id);
+    }
+
+    private getCurrentSize() {
+        return this.cards.values().reduce((total, card) => 
+            total + card.selected,
+            0);
+    }
+
+    public getRemainingSize() {
+        return Math.max(0, DECK_SIZE - this.getCurrentSize());
+    }
+
+    public isComplete() {
+        return this.getRemainingSize() === 0;
     }
 }
