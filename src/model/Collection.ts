2ea9e00971fc6553bcd49802e1c194fd6a3a3cfe
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-07-17 21:18:41 +0200

    Ajout de logs

--- src/model/Collection.ts
+++ src/model/Collection.ts
@@ -32,7 +32,10 @@ export function importDreambornCollection(exported: DreambornCollection, existin
     existingCards.forEach(card => setOwned(card.id, 0));
     // remplacer par la collection lue
     exported.forEach(expCard => {
+        console.log("Recherche de setCode " + expCard.setCode + " et number " + expCard.number);
         const knownCard = existingCards.find(card => card.setCode === expCard.setCode && card.number === expCard.number);
+        console.log("carte trouvée");
+        console.log(knownCard);
         if (knownCard !== undefined) {
             console.log("Enregistrement de " + expCard.owned + " cartes " + knownCard.name);
             setOwned(knownCard.id, expCard.owned);
