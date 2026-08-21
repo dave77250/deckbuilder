8527163a1305aced58d47d1d8e3aabf52221d262
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-08-21 09:44:18 +0200

    Conservation du texte cherché

--- src/components/CardGridToolBar.tsx
+++ src/components/CardGridToolBar.tsx
@@ -1,14 +1,16 @@
 import { FlexBox, FlexBoxDirection, FlexBoxJustifyContent, Input, Select, Option } from "@ui5/webcomponents-react";
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export type CardGridToolBarProps = {
     nbCols: number,
+    searchText: string,
     onSearch: (searchedText: string) => void,
     onColumnNbChange: (nbCols: number) => void,
     extraToolBarComponent?: ReactNode
 };
 
 export function CardGridToolBar(props: CardGridToolBarProps) {
+    const [search, setSearch] = useState(props.searchText);
     let searchTimeout = undefined;
     const COL_OPTIONS = ['1 col', '2 cols', '3 cols', '4 cols', '5 cols', '6 cols'];
     const onColsChange = (newCols: string) => {
@@ -22,6 +24,7 @@ export function CardGridToolBar(props: CardGridToolBarProps) {
             clearTimeout(searchTimeout);
         }
         setTimeout(() => {
+            setSearch(text);
             props.onSearch(text);
         }, 400);
     };
@@ -32,7 +35,7 @@ export function CardGridToolBar(props: CardGridToolBarProps) {
     });
     return (
         <FlexBox direction={FlexBoxDirection.Row} fitContainer={true} justifyContent={FlexBoxJustifyContent.SpaceBetween}>
-            <Input showClearIcon={true} placeholder="Chercher une carte" onInput={(event) => onTextTyped(event.target.value)}></Input>
+            <Input showClearIcon={true} placeholder="Chercher une carte" value={search} onInput={(event) => onTextTyped(event.target.value)}></Input>
             <FlexBox direction={FlexBoxDirection.Row}>
                 <Select onChange={(event) => onColsChange(event.target.value)}>
                     {COL_OPTIONS.map((c, i) => <Option key={c} selected={i === props.nbCols - 1}>{c}</Option>)}
